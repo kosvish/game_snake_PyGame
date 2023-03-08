a5afb165d926627225970c6acac2f43edaf72622
kosvish <mr.kocta@bk.ru>
2023-03-08 12:06:48 +0300

    feat(settings): add global settings for game

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+import pygame
+
+pygame.init()
+
+# Global variables
+WIDTH = 800
+HEIGHT = 600
+FPS = 5
+
+
+# Game Settings
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Змейка")
+clock = pygame.time.Clock()
+
+
