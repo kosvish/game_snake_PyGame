6f987be814b1c09a5aabb5cc3c2a30fb03ca6e50
kosvish <mr.kocta@bk.ru>
2023-03-10 11:29:42 +0300

    feat(create): func eat food, condition to lose,

--- main.py
+++ main.py
@@ -39,10 +39,12 @@ foodx = random.randrange(0, WIDTH - snake_block)
 foody = random.randrange(0, HEIGHT - snake_block)
 
 
-
-
-
-
+def eating_check(xcor, ycor, foodx, foody):
+    if foodx - snake_block <= xcor <= foodx + snake_block:
+        if foody - snake_block <= ycor <= foody + snake_block:
+            return True
+    else:
+        return False
 
 
 run = True
@@ -53,8 +55,16 @@ while run:
 
     screen.fill("blue")
 
+    # drawing food
     pygame.draw.rect(screen, "green", [foodx, foody,
                                        snake_block, snake_block])
+
+    # losing
+
+    if x1 >= WIDTH or x1 < 0 or y1 >= HEIGHT or y1 < 0:
+        run = False
+
+    # change cords
 
     x1 += x1_change
     y1 += y1_change
@@ -68,6 +78,15 @@ while run:
     for x in snake_list:
         pygame.draw.rect(screen, "black", [x[0], x[1],
                                            snake_block, snake_block])
+
+    for x in snake_list[:-1]:
+        if x == snake_head:
+            run = False
+
+    if eating_check(x1, y1, foodx, foody):
+        foodx = random.randrange(0, WIDTH - snake_block)
+        foody = random.randrange(0, HEIGHT - snake_block)
+        length += 1
 
     pygame.display.update()
     pygame.display.flip()
@@ -93,6 +112,5 @@ while run:
             elif event.key == pygame.K_UP:
                 x1_change = 0
                 y1_change = -snake_step
-
 
 pygame.quit()
